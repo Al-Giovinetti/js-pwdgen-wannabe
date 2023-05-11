63048f19dae6e12769381cf28db7d1404ba6fdf5
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-11 17:04:49 +0200

    Password con aggiunta di un numero x

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,31 @@
 /*Chiedi all’utentessa il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
-Infine scrivi sulla pagina nomecognomecolorepreferito21*/
+poi chiedi un numero e sommalo+10
+Infine scrivi sulla pagina nomecognomecolorepreferitonumero*/
 
-//Creo la variabile che sarà il risultato dell' interrogazione all' utente//
-const userName=prompt("insert your name")
+//Creo la variabile che sarà il risultato della prompt all' utente//
+const userName=prompt("insert your name");
 
-//Creo la variabile che sarà il risultato dell' interrogazione all' utente//
-const userSurname=prompt("insert your surname")
+//Creo la variabile che sarà il risultato della prompt all' utente//
+const userSurname=prompt("insert your surname");
 
-//Creo la variabile che sarà il risultato dell' interrogazione all' utente//
-let userBestColor=prompt("insert your favorite color")
+//Creo la variabile che sarà il risultato della prompt all' utente//
+let userBestColor=prompt("insert your favorite color");
+
+//Creo la variabile che sarà il risultato della prompt all'utente//
+//La prompt dell' utente è una stringa e va trasformata in un intero per sommarla con 10//
+
+let userRandomNumber= parseInt(prompt("Insert a number, we will add + 10"));
+
+userRandomNumber=(userRandomNumber+10);
 
 //Creo la variabile che sarà la nuova password
 
-let userNewPassword= userName + userSurname + userBestColor + "21" 
+let userNewPassword= userName + userSurname + userBestColor + userRandomNumber; 
 
 // stampo la variabile tramite il suo ID
 
-document.getElementById("new-password").innerHTML= userNewPassword
+document.getElementById("new-password").innerHTML= userNewPassword;
+
+console.log(userNewPassword);
