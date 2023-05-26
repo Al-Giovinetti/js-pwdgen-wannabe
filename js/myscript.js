067d426afa4a30097f88ee2ee9e82ae146157a02
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-26 21:36:35 +0200

    Aggiornato

--- js/myscript.js
+++ js/myscript.js
@@ -4,31 +4,22 @@ poi chiedi il suo colore preferito
 poi chiedi un numero e sommalo+10
 Infine scrivi sulla pagina nomecognomecolorepreferitonumero*/
 
-//Creo la variabile che sarà il risultato della prompt all' utente//
-const userName=prompt("insert your name");
+const userName = document.getElementById("user-name")
+const userSurname = document.getElementById("user-surname")
+const userColor = document.getElementById("user-color")
+const userNumber = document.getElementById("user-number")
 
-//Creo la variabile che sarà il risultato della prompt all' utente//
-const userSurname=prompt("insert your surname");
+const btnGenerates = document.querySelector("button")
 
-//Creo la variabile che sarà il risultato della prompt all' utente//
-let userBestColor=prompt("insert your favorite color");
+const output= document.getElementById("new-password")
 
-//Creo la variabile che sarà il risultato della prompt all'utente//
-//La prompt dell' utente è una stringa e va trasformata in un intero per sommarla con 10//
+btnGenerates.addEventListener("click",function(){
+    let sum=parseInt(userNumber.value)+10
+    output.innerHTML=userName.value + userSurname.value + userColor.value + sum
 
-let userRandomNumber= parseInt(prompt("Insert a number, we will add + 10"));
-console.log(typeof(userRandomNumber));
+    userName.value=" "
+    userSurname.value=" "
+    userColor.value=" "
+    userNumber.value=" "
+})
 
-userRandomNumber=(userRandomNumber+10);
-console.log(typeof(userRandomNumber));
-
-//Creo la variabile che sarà la nuova password
-
-let userNewPassword= userName + userSurname + userBestColor + userRandomNumber; 
-console.log(typeof(userRandomNumber));
-
-// stampo la variabile tramite il suo ID
-
-document.getElementById("new-password").innerHTML= userNewPassword;
-
-console.log(userNewPassword);
